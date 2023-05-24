46b76fcf2957297f2641d9c4dca0319c7e3c7dd8
Abdelrahman shaheen <a.shaheen20001@gmail.com>
2023-05-24 13:15:32 +0000

    Home-page

---
+++ src/Component/Home.js
@@ -0,0 +1,16 @@
+import GameDeals from "./GameDeals";
+import NavBars from "./NavBars";
+import Poster from "./Poster";
+
+function Home (){
+    return(
+        <>
+        <NavBars/>
+    <Poster/>
+    <GameDeals/>
+        </>
+    )
+
+}
+
+export default Home;

4fed205e620a08a7a6caf06a5aa2d97c2b1dfdaa
osamaalkarmi20 <osama.alkarmi20@outlook.com>
2023-05-27 15:33:42 +0300

    news frontend remake

--- src/Component/News.js
+++ src/Component/News.js
@@ -1,43 +1,71 @@
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
+
 import './news.css';
-import Nav from './NavBars'
+import Nav from './NavBars';
+import { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import { CardGroup } from 'react-bootstrap';
+import { MDBCard, MDBCardImage, MDBCardOverlay, MDBCardText, MDBCardTitle, MDBRipple } from 'mdb-react-ui-kit';
+
+   
+
+
+
 function News (){
+  const [moviesdata, setMoviesData] = useState([])
+
+  const getAllMovies = () => {
+    const serverURL = `http://localhost:3005/trending`;
+
+ 
+    fetch(serverURL)
+        .then(response => {
+            response.json().then(data => {
+           
+                setMoviesData(data)
+               
+
+            })
+        })
+}
+
+     
+useEffect(()=>{
+    getAllMovies()
+},[])
+
    
     return(
         <>
+    
+    
         <Nav/>
-        <img className='mainImg'src='https://media-cldnry.s-nbcnews.com/image/upload/t_focal-758x379,f_auto,q_auto:best/rockcms/2022-10/121017-bayonetta-se-1218p-bea611.jpg' alt='img' />
-        <div>
-          <h3>Title of the News</h3>
-         
-          <p>News Discr</p>
-         
-          <Col xs={6} md={4}>
-          <Image className='imgClass'src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg" roundedCircle />
-        </Col>
-        <h4>author Name:...........</h4>
-          <Button className='buttonnews' variant="info" onClick={()=>{
+       
 
-          }}>More Information</Button>{' '}
-   
-        </div>
-        <div>
-          <h3>Title of the News</h3>
+        <div ></div>
+        {moviesdata.map(item => {
+          if (!item.urlToImage) {
+            return null; 
+          }
+                return (
          
-          <p>News Discr</p>
-         
-          <Col xs={6} md={4}>
-          <Image   className='imgClass' src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg" roundedCircle />
-        </Col>
-        <h4>author Name:...........</h4>
-          <Button   className='buttonnews' variant="info" onClick={()=>{
-
-          }}>More Information</Button>{' '}
-   
-        </div>
+                  <a href={item.url}>      <CardGroup >  <MDBCard background='dark' className='text-white'>
+   <MDBCardImage overlay src={item.urlToImage} alt='...' /> <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}></div>
+      <MDBCardOverlay>
      
+   <MDBCardTitle style={{ textAlign:"center"}}> <h1>{item.title}</h1></MDBCardTitle>
+        <MDBCardText style={{ textAlign:"center"}}>
+       <p> {item.description}</p>
+        </MDBCardText>
+       
+      </MDBCardOverlay>
+    </MDBCard>
+    </CardGroup>   </a>
+                )
+
+
+            })}
+      
+          
         </>
     )
 }

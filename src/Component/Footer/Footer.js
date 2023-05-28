91fe1c1e8552cbf7cdb821b0a5fd6baafb39468a
bashar-27 <1934674@std.hu.edu.jo>
2023-05-28 22:34:30 +0000

    update

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -4,7 +4,7 @@ import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
 export default function Footer() {
   return (
     <MDBFooter className='text-center text-white' style={{bottom:"0" }}>
-      <MDBContainer className='p-4'></MDBContainer>
+   
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2023 Copyright:
